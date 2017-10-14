bcc5efa051459c3e6969f43f0916ac3665c6c8c2
dalhun namgung <bnamgung@gmail.com>
2017-10-14 13:54:29 -0700

    mid done

--- test/fp-test.js
+++ test/fp-test.js
@@ -13,3 +13,27 @@ describe('test map using cb', () => {
   });
 });
 //test map
+
+describe('test filter using cb', () => {
+  it('should return all id * 2', () => {
+    let results = fp.filter([1,2,3,4,5,6], a => a % 2 ==== 0);
+    expect (results).toEqual([2,4,6]);
+  });
+});
+//test filter
+
+describe('test reduce using cb', () => {
+  it('should return all id * 2', () => {
+    let results = fp.reduce([1,2,3,4,5], (x + y);
+    expect(results).toEqual(15);
+  });
+});
+//test reduce
+
+describe('test splice using apply', () => {
+  it('should return two lists combined', () => {
+    let results = fp.concat([1,2,3], [4,5,6]);
+    expect(results).toEqual([1,2,3,4,5,6]);
+  });
+});
+//test splice
